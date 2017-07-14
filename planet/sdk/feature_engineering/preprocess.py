1e617b8d02cc5f481e9c50e00418c2d7a334991b
ryangoy <rgoy@berkeley.edu>
2017-07-14 10:18:23 -0700

    Update preprocess.py

--- planet/sdk/feature_engineering/preprocess.py
+++ planet/sdk/feature_engineering/preprocess.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-from os.path import join
+from os.path import join, exists
+from os import makedirs
 import sys
+from shutil import copyfile, rmtree
 
 # TO DO per dataset:
 # - change the clean_data method to whatever is needed
@@ -19,15 +21,31 @@ def one_hot_vectorize_labels(ds_dir):
     train = pd.read_csv(join(ds_dir, 'processed/train.csv'))
     return
 
-def generate_binary_classifier_structure(train_folder, feature_name):
+# UNTESTED
+def generate_binary_data_structure(train_folder, target_folder, train_df, feature_name,
+                                   extension='.jpg'):
     """
-    Copies data from original data to another folder where each subfolder
-    is the label.
+    Copies data from train_folder to another folder where it puts images into two subfolders:
+    one for positive images for feature_name and one for negatives.
     """
-    orig_folder = join(ds_dir, 'orig/train-tif-v2')
-    target_folder = join(ds_dir, 'binary')
-    return
-
+    # delete existing folders
+    if exists(target_folder):
+        rmtree(target_folder)
+    os.makedirs(target_folder)
+    negatives_dir = join(target_folder, 'not_'+feature_name)
+    positives_dir = join(target_folder, feature_name)
+    os.makedirs(negatives_dir)
+    os.makedirs(positives_dir)
+  
+    # sort each picture into its respective directory
+    for row in train_df:
+        if row[feature_name] == 1:
+            dest_dir = positives_dir
+        else:
+            dest_dir = negatives_dir
+        copyfile(join(train_folder, row.image_name+extension), 
+                 join(target_folder, dest_dir))
+        
 def import_data():
     train = pd.read_csv(join(ds_dir, 'train.csv'))
     test = pd.read_csv(join(ds_dir, 'test.csv'))
